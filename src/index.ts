d7d47d99e6647b51a215c47b941550e2a26f7fb4
HansM7 <melchorhans5@gmail.com>
2024-08-24 12:06:56 -0500

    [feat] create config import w alias @

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
+import "module-alias/register";
+
 import express from "express";
 import { envConfig } from "./config/env.config";
 import userRouter from "./user/user.init";
+
 const globalPrefix = "/api";
+
 const app = express();
+
 app.use(express.json());
+
 app.use(globalPrefix, userRouter);
+
 app.listen(envConfig.port, () => {
   console.log(`listening on port ${envConfig.port}`);
 });
