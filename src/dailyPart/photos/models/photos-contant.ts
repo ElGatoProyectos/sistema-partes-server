848aa0125da676142282a7b8a540a72af4aee284
Geronimo Luminari <geronimoluminari3@outlook.com>
2024-11-13 15:38:50 -0300

    [feat] add filter todos in assists present

---
+++ src/dailyPart/photos/models/photos-contant.ts
@@ -0,0 +1,16 @@
+export const DailyPartPhotoOneMulterFile = {
+  folder: "dailyPartPhotos",
+  field: "photo-one",
+};
+export const DailyPartPhotoTwoMulterFile = {
+  folder: "dailyPartPhotos",
+  field: "photo-two",
+};
+export const DailyPartPhotoThreeMulterFile = {
+  folder: "dailyPartPhotos",
+  field: "photo-three",
+};
+export const DailyPartPhotoFourMulterFile = {
+  folder: "dailyPartPhotos",
+  field: "photo-four",
+};
